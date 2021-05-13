22b631bbe78cb64566cc4a787140e18b6f56411b
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-13 22:32:20 +0300

    working on footer

--- osf-react/src/components/Footer/Footer.js
+++ osf-react/src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer() {
     return(
     <section>
-       <>
-       </>
+       
+       
     </section>
     
         
--- osf-react/src/components/NavbarMenu/NavbarMenu.js
+++ osf-react/src/components/NavbarMenu/NavbarMenu.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Navbar,Nav,NavDropdown, Image } from 'react-bootstrap';
+import { Navbar,Nav,NavDropdown, Image, Accordion, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavbarMenu.scss';
 import './NavbarMenu.css';
 import Hamburger from '../../../node_modules/hamburger-react';
 import logo from '../Img/logo.webp';
 import NavImage from '../Img/dropdown-img.webp';
-
+import Search from '../Img/search.png';
+import ProfileUser from '../Img/profileUser.png'; 
+import Heart from '../Img/Heart.png';
+import BagForShopping from '../Img/BagForShopping.png';
 
 function NavbarMenu() {
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" sticky="top">
             <Navbar.Toggle aria-controls="basic-navbar-nav" >
                 <Hamburger></Hamburger>
             </Navbar.Toggle>
@@ -18,9 +21,9 @@ function NavbarMenu() {
                 <img className="logoImg" src={logo} alt="Logo" />
             </Navbar.Brand>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <NavDropdown title="SERVICES" id="basic-nav-dropdown">
-                            <NavDropdown title="PRODUCT CATEGORIES" id="nav-drop-categ">
+                <Nav className="mr-auto" sticky="none">
+                    <NavDropdown title="SERVICES" id="basic-nav-dropdown" sticky="none">
+                            <NavDropdown title="PRODUCT CATEGORIES" id="nav-drop-categ" sticky="none">
                                 <NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item>
@@ -47,7 +50,7 @@ function NavbarMenu() {
                                 <NavDropdown.Item href="#action/3.24">Toys</NavDropdown.Item>
                             </NavDropdown>
                         
-                            <NavDropdown title="SALE" id="nav-drop-categ">
+                            <NavDropdown title="SALE" id="nav-drop-categ" sticky="none">
                                 <NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item>
@@ -58,14 +61,71 @@ function NavbarMenu() {
                                 <NavDropdown.Item href="#action/3.8">Food</NavDropdown.Item>
                             </NavDropdown>
                         </NavDropdown>
-                        <NavDropdown title="COMPANY" id="basic-nav-dropdown"></NavDropdown>
-                        <NavDropdown title="LIBRARY" id="basic-nav-dropdown"></NavDropdown>          
-                        <NavDropdown title="CONTACT US" id="basic-nav-dropdown"></NavDropdown>
-                        <Image src={NavImage} fluid/>
+                        <NavDropdown title="COMPANY" id="basic-nav-dropdown" sticky="none"></NavDropdown>
+                        <NavDropdown title="LIBRARY" id="basic-nav-dropdown" sticky="none"></NavDropdown>          
+                        <NavDropdown title="CONTACT US" id="basic-nav-dropdown" sticky="none"></NavDropdown>
+                        <Image src={NavImage} fluid sticky="none"/>
                 </Nav>
                 
             </Navbar.Collapse>
+            <div className="footerDetails">
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                CONTACT
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                            <div>
+                                <h5>Headquarters:</h5>
+                                <h5>5600, Blvd. des Galeries, Bur 530</h5>
+                                <h5>Quebeb, Quebec G2K 2H6</h5><br />
+                                <h5>contact@osf-global.com</h5><br />
+                                <h5>+1 (888) 548-4344</h5>
+                            </div>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                                CATEGORIES
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                                ABOUT
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="3">
+                            <Card.Body>Hello! I'm another body</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            </div>
+            
+            <Navbar expand="lg" id="navBarBottom">
+                <div className="flex1">
+                <div className="numberFooter">
+                    <h1>0</h1>
+                </div>  
+                <div className="numberFooter">
+                    <h1>0</h1>
+                </div> 
+                </div>     
+                <Image  className="imgFooter" src={Search} />
+                <Image  className="imgFooter" src={ProfileUser} />
+                <Image  className="imgFooter" src={Heart} />
+                <Image  className="imgFooter" src={BagForShopping} />
+            </Navbar>
+            
         </Navbar>
+        
     );
 }
 
