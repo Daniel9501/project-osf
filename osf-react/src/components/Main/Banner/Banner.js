bcc2aa2440d05005108b2a3260f13c4ab62bd8d9
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-19 23:28:50 +0300

    Done with Banner components

--- osf-react/src/components/Main/Banner/Banner.js
+++ osf-react/src/components/Main/Banner/Banner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './Banner.css';
-
+import Banner1 from '../../Img/Main/Banner/img1.webp';
 function Banner() {
     return (
-        <div>
-            <h1>Banner</h1>
+        <div className="mainBanner">
+            <img src={Banner1} alt="" />
+            <div className="mainTitle">Banner OSF Theme</div>               
+            <div className="mainText">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</div>               
         </div>
     );
   }
