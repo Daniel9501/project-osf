118ff53d1d55693b3c087544cd0f156b7b192e47
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-20 18:03:02 +0300

    I only display CategoryLandingPage

--- osf-react/src/index.js
+++ osf-react/src/index.js
@@ -2,12 +2,14 @@ import React from '../node_modules/react';
 import ReactDOM from '../node_modules/react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+// import HomePage from './HomePage.js';
+import CategoryLandingPage from './CategoryLandingPage.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <HomePage /> */}
+    <CategoryLandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
