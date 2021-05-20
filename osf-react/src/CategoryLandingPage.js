023ea4ebb50f0da703d7021d75d7a92e3f4bde1a
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-20 18:08:59 +0300

    create Main page for CategoryLandingPage

---
+++ osf-react/src/CategoryLandingPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import NavbarMenu from './components/NavbarMenu/NavbarMenu.js';
+import Footer from './components/Footer/Footer.js';
+import Pagesfooter from './components/PagesFooter/PagesFooter.js';
+import ClpBreadCrumb from './components/CLPMobile/BreadCrumb/ClpBreadCrumb.js'
+import ClpServices from './components/CLPMobile/Title/ClpServices.js'
+import './CategoryLandingPage.css';
+import Cards from './components/CLPMobile/Cards/Cards.js';
+function CategoryLandingPage() {
+  return (
+    <div className="entirePage">
+        <div className="navigationMenu">
+            <NavbarMenu />
+        </div>
+        <div className="mainPage">
+            <ClpBreadCrumb />
+            <ClpServices />
+            <Cards />
+            <Pagesfooter />
+            <Footer /> 
+        </div>
+         
+    </div>
+  );
+}
+
+export default CategoryLandingPage;
