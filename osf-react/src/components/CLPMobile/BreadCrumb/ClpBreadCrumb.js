26a837b51eb709c12ef06a8fce4f5862864f1dfa
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-20 18:08:14 +0300

    Create BreadCrumb folder and inside found js and css file

---
+++ osf-react/src/components/CLPMobile/BreadCrumb/ClpBreadCrumb.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ClpBreadCrumb.css';
+
+function ClpBreadCrumb() {
+    return (
+        <div>
+            <h6>ClpBreadCrumb</h6>
+        </div>
+    );
+  }
+
+export default ClpBreadCrumb;
