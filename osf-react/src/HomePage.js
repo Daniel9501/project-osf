29938e24965f95b96783eda7e06de85f1b7b8041
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-20 18:01:23 +0300

    I changed the App name to HomePage

---
+++ osf-react/src/HomePage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import NavbarMenu from './components/NavbarMenu/NavbarMenu.js';
+import Footer from './components/Footer/Footer.js';
+import Pagesfooter from './components/PagesFooter/PagesFooter.js';
+import Slider from './components/Main/Slider/Slider.js';
+// import Sales from './components/Main/Sales/Sales.js';
+import Services from './components/Main/Services/Services.js';
+import Banner from './components/Main/Banner/Banner.js';
+import Products from './components/Main/Products/Products.js';
+// import LatestPosts from './components/Main/LatestPosts/LatestPosts.js';
+
+import './HomePage.css';
+function HomePage() {
+  return (
+    <div className="entirePage">
+      <div className="navigationMenu">
+        <NavbarMenu />
+      </div>
+
+      <div className="mainPage">
+        <div className="firstRow">
+          <Slider />
+          {/* <Sales /> */}
+        </div>
+        <Products />
+        <Banner />
+        {/* <LatestPosts /> */}
+        <Services />
+        <Pagesfooter />
+      </div>
+      
+      <div className="footerPageMobile">
+        <Footer /> 
+      </div>
+         
+    </div>
+  );
+}
+
+export default HomePage;
