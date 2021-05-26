02fe2ba411ffacd7d84f2c1670a4e35063f833a8
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-26 23:58:27 +0300

    add style and start to implement desktop version

--- osf-react/src/pages/HomePage.js
+++ osf-react/src/pages/HomePage.js
@@ -4,12 +4,11 @@ import NavbarMenu from '../components/NavbarMenu/NavbarMenu.js';
 import Footer from '../components/Footer/Footer.js';
 import Pagesfooter from '../components/PagesFooter/PagesFooter.js';
 import Slider from '../components/Main/Slider/Slider.js';
-// import Sales from './components/Main/Sales/Sales.js';
+import Sales from '../components/Main/Sales/Sales.js';
 import Services from '../components/Main/Services/Services.js';
 import Banner from '../components/Main/Banner/Banner.js';
 import Products from '../components/Main/Products/Products.js';
 import Cookies from '../components/Cookies/Cookies.js';
-// import LatestPosts from './components/Main/LatestPosts/LatestPosts.js';
 
 import './HomePage.css';
 
@@ -24,31 +23,40 @@ function HomePage() {
   },[]);
 
   return (
-    <div className="entirePage">
-      <Cookies trigger={buttonCookies} setTrigger={setButtonCookies}></Cookies>
-      <Cookies trigger={timedCookies} setTrigger={setTimedCookies}></Cookies>
-      <div className="navigationMenu">
-        <NavbarMenu />
-      </div>
-
-      <div className="mainPage">
-        <div className="firstRow">
-          <Slider />
-          {/* <Sales /> */}
+    <div>
+      <div className="entirePageMobile">
+        <Cookies trigger={buttonCookies} setTrigger={setButtonCookies}></Cookies>
+        <Cookies trigger={timedCookies} setTrigger={setTimedCookies}></Cookies>
+        <div className="navigationMenu">
+          <NavbarMenu />
         </div>
-        <Products />
-        <Banner />
-        {/* <LatestPosts /> */}
-        <Services />
-        <Pagesfooter />
+
+        <div className="mainPage">
+          <div className="firstRow">
+            <Slider />
+          </div>
+          <Products />
+          <Banner /> 
+          <Services />
+          <Pagesfooter />
+        </div>
+        <div className="footerPageMobile">
+          <Footer /> 
+        </div>
       </div>
-      
-      <div className="footerPageMobile">
-        <Footer /> 
+      <div className="entirePageDesktop">
+        <Cookies trigger={buttonCookies} setTrigger={setButtonCookies}></Cookies>
+        <Cookies trigger={timedCookies} setTrigger={setTimedCookies}></Cookies>
+        <div className="firstRow">
+          <NavbarMenu />
+        </div>
+        <div className="secondRow">
+          <Sales />
+        </div>
       </div>
-         
     </div>
   );
 }
+
 
 export default HomePage;
