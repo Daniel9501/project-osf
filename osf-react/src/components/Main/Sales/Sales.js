506f04094789a3a1d3aa10f6d56cafa918fe8702
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-26 23:56:02 +0300

    add styles

--- osf-react/src/components/Main/Sales/Sales.js
+++ osf-react/src/components/Main/Sales/Sales.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import Button from '../../../../node_modules/@material-ui/core/Button';
 import './Sales.css';
+
 import ImgSales from '../../Img/Main/salesImage.webp';
 import Facebook from '../../Img/Socialicons/f.png';
+
+
 function Sales() {
     return (
         <div className="sales">
             <div className="image">
-                <img src={ImgSales} alt="salesImage" />
+                <img className="imageSales" src={ImgSales} alt="salesImage" />
+                <h1 className="textBig">55%</h1>
+                <h3 className="textTitle">SUMMER<br />SALES</h3>
             </div> 
             <div className="text">
-                <h1>55%</h1>
-                <h3>SUMMER<br />SALES</h3>
-                <h4>Follow us on Facebook</h4>
-                <h5>Sed ut perspiciatis unde omnis iste<br />natus error sit </h5>
+                <h4 className="textText">Follow us on Facebook</h4>
+                <h5 className="textDetails">Sed ut perspiciatis unde omnis iste<br />natus error sit </h5>
             </div>
             <div className="facebookButton">
                 <Button variant="outlined" color="primary" href="https://www.facebook.com/">
                     <img src={Facebook} alt="facebook" />
-                    Follow
+                    <span className="facebookText">Follow</span>
                 </Button>
             </div>
         </div>
