ccf53fc0c38428e5a367dd34c18166a560e0a509
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-26 16:46:55 +0300

    Link to 404 Page

--- osf-react/src/components/NavbarMenu/NavbarMenu.js
+++ osf-react/src/components/NavbarMenu/NavbarMenu.js
@@ -19,48 +19,48 @@ function NavbarMenu() {
                 </Navbar.Toggle>
 
                 <Navbar.Brand href="#home" className="logo">
-                    <Link to="/CategoryLanding"><img className="logoImg" src={logo} alt="Logo" /></Link>
+                    <Link to="/Home"><img className="logoImg" src={logo} alt="Logo" /></Link>
                 </Navbar.Brand>
                 
                 <Navbar.Collapse id="basic-navbar-nav">{/* Dropdown when press on Hamburger Menu */}
                     <Nav className="mr-auto" sticky="none">
                         <NavDropdown title="SERVICES" id="basic-nav-dropdown" sticky="none">
                             <NavDropdown title="PRODUCT CATEGORIES" id="nav-drop-categ" sticky="none">
-                                <NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Books</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.5">Drink</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.6">Electronics</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.7">Flower & Plants</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.8">Food</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.9">Gadgets</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.10">Garden</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.11">Grocery</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.12">Home</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.13">Jewelry</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.14">Kids & Baby</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.15">Men's Fashion</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.16">Mobile</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.17">Motorcycles</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.18">Movies</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.19">Music</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.20">Ofiice</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.21">Pets</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.22">Romantic</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.23">Sport</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.24">Toys</NavDropdown.Item>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.4">Books</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.5">Drink</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.6">Electronics</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.7">Flower & Plants</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.8">Food</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.9">Gadgets</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.10">Garden</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.11">Grocery</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.12">Home</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.13">Jewelry</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.14">Kids & Baby</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.15">Men's Fashion</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.16">Mobile</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.17">Motorcycles</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.18">Movies</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.19">Music</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.20">Ofiice</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.21">Pets</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.22">Romantic</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.23">Sport</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.24">Toys</NavDropdown.Item></Link>
                             </NavDropdown>
                         
                             <NavDropdown title="SALE" id="nav-drop-categ" sticky="none">
-                                <NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.4">Books</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.5">Drink</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.6">Electronics</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.7">Flower & Plants</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.8">Food</NavDropdown.Item>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.1">Accessories</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.2">Alcohol</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.3">Art</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.4">Books</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.5">Drink</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.6">Electronics</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.7">Flower & Plants</NavDropdown.Item></Link>
+                                <Link to="/404"><NavDropdown.Item href="#action/3.8">Food</NavDropdown.Item></Link>
                             </NavDropdown>
                         </NavDropdown>
                         <NavDropdown title="COMPANY" id="basic-nav-dropdown" sticky="none"></NavDropdown>
