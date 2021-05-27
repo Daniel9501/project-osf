d6506a2a4bcf8b1d1463668d2361b25c6a4e17f9
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-27 18:51:52 +0300

    update for desktop version

--- osf-react/src/components/Main/Slider/Slider.js
+++ osf-react/src/components/Main/Slider/Slider.js
@@ -3,10 +3,9 @@ import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
 import './Slider.css';
-import Img1 from '../../Img/Main/firstImage1.webp';
-import Img2 from '../../Img/Main/firstImage2.webp';
-import Img3 from '../../Img/Main/firstImage3.webp';
-import Img4 from '../../Img/Main/firstImage4.webp';
+import Img1 from '../../Img/Desktop/Slider/img1.webp';
+import Img2 from '../../Img/Desktop/Slider/img2.webp';
+import Img3 from '../../Img/Desktop/Slider/img3.webp';
 
 function Slider() {
     const [index, setIndex] = React.useState(0);
@@ -22,8 +21,7 @@ function Slider() {
                     <img className="d-block w-100 firstImage" src={Img1} alt="Img1" />
                     <Carousel.Caption>
                         <h4 className="title">Control and manage any device with cloud solutions</h4>
-                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes<br />
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed</p>
+                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes</p>
                         <div className="buttonView">
                             <Link to='/CategoryLanding'><button className="viewMoreButton" href="#">VIEW MORE</button></Link>
                         </div>
@@ -33,8 +31,7 @@ doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veri
                     <img className="d-block w-100 firstImage" src={Img2} alt="Img2" />
                     <Carousel.Caption>
                         <h4 className="title">Control and manage any device with cloud solutions</h4>
-                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes<br />
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed</p>
+                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes</p>
                         <div className="buttonView">
                             <Link to='/CategoryLanding'><button className="viewMoreButton" href="#">VIEW MORE</button></Link>
                         </div>
@@ -44,19 +41,7 @@ doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veri
                     <img className="d-block w-100 firstImage" src={Img3} alt="Img3" />
                     <Carousel.Caption>
                         <h4 className="title">Control and manage any device with cloud solutions</h4>
-                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes<br />
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed</p>
-                        <div className="buttonView">
-                            <Link to='/CategoryLanding'><button className="viewMoreButton" href="#">VIEW MORE</button></Link>
-                        </div>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img className="d-block w-100 firstImage" src={Img4} alt="Img4" />
-                    <Carousel.Caption>
-                        <h4 className="title">Control and manage any device with cloud solutions</h4>
-                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes<br />
-doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed</p>
+                        <p className="content">Improve business performance and the user experience with the right mix of IoT technology and processes</p>
                         <div className="buttonView">
                             <Link to='/CategoryLanding'><button className="viewMoreButton" href="#">VIEW MORE</button></Link>
                         </div>
