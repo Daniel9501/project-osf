97e2459b6a0dbec4d12280c1541273d19e5ad100
Daniel9501 <stoicadaniel9501@gmail.com>
2021-05-30 23:15:25 +0300

    Update Cookies for desktop

--- osf-react/src/components/Cookies/Cookies.js
+++ osf-react/src/components/Cookies/Cookies.js
@@ -10,8 +10,10 @@ function Cookies(props) {
           <img src={x} alt=""></img>
         </button>
         <h3>This website uses cookies</h3><br />
-        <p>OSF uses its own and third-party cookies for statical purposes, to know your preferences, for website performance and interaction with social media offering publicity tailored to your interests. If you continue
-          browsing, we consider that you accept its use.<br />You cand expand this information consulting our <span>Cookies Policy Page</span>.
+        <p>OSF uses its own and third-party cookies for statical purposes, to know your preferences, for website 
+          performance<br />and interaction with social media offering publicity tailored to your interests. If you continue
+          browsing, we consider that you accept its use. You cand expand this information consulting our 
+          <span>Cookies Policy Page</span>.
         </p>
         <button className="acceptButton" onClick={() => props.setTrigger(false)}><h4>ACCEPT</h4></button>
         {props.children}
